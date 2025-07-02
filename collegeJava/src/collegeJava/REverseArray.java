7f6b21aa700fecc26a68ce4e3eb4196a9292b71b
pimpa <pimpa@SWAPANALI>
2025-07-02 15:09:21 +0530

    day 3

---
+++ collegeJava/src/collegeJava/REverseArray.java
@@ -0,0 +1,38 @@
+package collegeJava;
+
+import java.util.Scanner;
+
+public class REverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner c=new Scanner(System.in);
+		System.out.println("Enter your array size");
+		int size=c.nextInt();
+		
+		int arr[]=new int[size];
+		System.out.println("Enter elements");
+		for(int i=0;i<size;i++) {
+			arr[i]=c.nextInt();
+		}
+		
+		for(int i=0;i<size;i++) {
+			System.out.println(arr[i]);
+		}
+		int i=0;
+		int end=size-1;
+		while(i<=end) {
+			int temp=arr[i];
+			arr[i]=arr[end];
+			arr[end]=temp;
+			
+			i++;
+			end--;
+		}
+		for(int j=0;j<size;j++) {
+			System.out.println(arr[j]);
+		}
+	}
+
+}
