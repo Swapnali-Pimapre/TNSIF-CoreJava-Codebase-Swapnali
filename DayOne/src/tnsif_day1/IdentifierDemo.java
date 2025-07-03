439dada817fd9338c6acda5d6205aa7eedb7d197
pimpa <pimpa@SWAPANALI>
2025-07-03 10:43:51 +0530

    day 1

---
+++ DayOne/src/tnsif_day1/IdentifierDemo.java
@@ -0,0 +1,23 @@
+package tnsif_day1;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// int for=13;  error bcoz of keyword cannot be declare as variables
+		//int num 1=13; error bcoz of space
+		//int @num=12;special characters are not allowed
+		//
+		//valid identification
+		
+		String $name="radha";
+		String StudentName="radha";
+		String For="India";
+		int marks=30;
+		long phone=9058769852l;
+		System.out.println("Name of Student is "+$name);
+		System.out.println("Marks of Student :"+marks);
+		}
+
+}
---
+++ DayOne/src/tnsif_day1/firstProgram.java
@@ -0,0 +1,11 @@
+package tnsif_day1;
+
+public class firstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+
+	}
+
+}
